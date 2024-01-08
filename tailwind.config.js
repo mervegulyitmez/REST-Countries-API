7d576e99649d6b7f142e26eee5224c8791ae176c
mervegulyitmez <merveyitmez@gmail.com>
2024-01-07 21:37:59 -0500

    Tailwind config file configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html", "./app/*.js"],
   theme: {
     extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1300px",
+    },
   },
   plugins: [],
-}
-
+};
